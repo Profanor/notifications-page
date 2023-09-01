a984fe2b774f26f5a09bc18a74c911f82f7feea4
kaneki <fivelanes72@gmail.com>
2023-09-01 14:57:20 +0100

    done styling

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 //grab the unread divs
 const mark1 = document.querySelector("webber");
+const mark2 = document.querySelector("angela");
+const mark3 = document.querySelector("jacob");
+const notifyMe = document.querySelector("number");
+const toggleMe = document.querySelector("toggle");
+
+//reset the number of notifications to zero
+function clearState() {
+    document.getElementById("number").innerHTML = 0;
+    document.getElementById("webber").style.backgroundColor = "white";
+    document.getElementById("angela").style.backgroundColor = "white";
+    document.getElementById("jacob").style.backgroundColor = "white";
+    document.getElementById("kim").style.color = "gray";
+}
